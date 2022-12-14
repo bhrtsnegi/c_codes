947d27f9cab71829560fee8678319b25bdb902ed
Bharat Negi <1bhrtsnegi@gmail.com>
2022-12-14 23:05:16 +0530

    Add files via upload
    Even Odd

---
+++ even_odd.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+    // program to find even or odd
+    int num, arr[26], count1, count;
+    count=0;
+    count1=0;
+    // to initialize number of elements in array
+    printf("Enter 25 positive integers: \n");
+    for(num=1; num<26; num++){
+        scanf("%d", &arr[num]);
+    }
+
+    //logic for even odd
+    for(num=1; num<26; num++){
+        if(arr[num]%2==0){
+            count++;
+            continue;
+        }
+        else{
+            count1++;
+            continue;
+        }
+    }
+    printf("Odd Numbers are %d", count1);
+    printf("\n Even Numbers are %d", count);
+}
