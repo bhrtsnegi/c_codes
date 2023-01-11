c61142cf1139a553c1330c8fbc99fd02dee24f6d
Bharat Negi <1bhrtsnegi@gmail.com>
2023-01-11 22:23:29 +0530

    Factorial

---
+++ factorial.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+ int fact=1,i,n;
+ printf("Enter a number to print it's factorial: ");
+ scanf("%d",&n);
+ for(i=1;i<=n;i++)
+ {
+  fact=fact*i;
+ }
+ printf("The factorial of %d is %d",n,fact);
+}
