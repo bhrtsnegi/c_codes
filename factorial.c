39ead321469d6357757615558c45ec1f3a908211
Bharat Negi <1bhrtsnegi@gmail.com>
2023-01-12 02:41:53 +0530

    Update factorial.c
    Updation done

--- factorial.c
+++ factorial.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
+// factorial code for any number answer is in range of integer
 int main()
 {
  int fact=1,i,n;
  printf("Enter a number to print it's factorial: ");
  scanf("%d",&n);
+
  for(i=1;i<=n;i++)
  {
   fact=fact*i;
  }
+
  printf("The factorial of %d is %d",n,fact);
+ return 0;
 }
