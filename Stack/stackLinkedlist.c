e86dc043a0a089f85ad14301901898de69b15833
Bharat Negi <1bhrtsnegi@gmail.com>
2023-02-24 11:55:20 +0530

    Add files via upload
    Completed

--- Stack/stackLinkedlist.c
+++ Stack/stackLinkedlist.c
@@ -7,71 +7,115 @@ struct node
     int info;
     struct node *link;
 };
-struct node *first= NULL;
+struct node *top= NULL;
 
 //push block
 int push()
 {
-    struct node *ptr, *nptr;
+    struct node *ptr;
     ptr= (struct node*)malloc(sizeof(struct node));
-    ptr= first;
-    if(first==NULL)
+    printf("Enter the value which you want to push: ");
+    scanf("%d", &ptr->info);
+    if(top==NULL)
     {
-        first=ptr;
-        printf("Enter the value which you want to push: ");
-        scanf("%d", &ptr->info);
+        top=ptr;
         ptr->link=NULL;
     }
     else
     {
-        nptr->link=ptr;
-        first=nptr;
-        printf("Enter the value which you want to push: ");
-        scanf("%d", nptr->info);
+        ptr->link=top;
+        top=ptr;
     }
 }
 
 //pop block
 int pop()
 {
-
+    int value;
+    if(top==NULL)
+    {
+        printf("Underflow. Back to main menu");
+    }
+    else
+    {
+        value= top->info;
+        top=top->link;
+        printf("%d deleted successfully.\n", value);
+    }
 }
 
 //element Search block
 int elementSearch()
 {
+    int val, count=0;
+    struct node *ptr;
+    ptr= (struct node*)malloc(sizeof(struct node));
+    ptr=top;
+    printf("\nEnter the value which you want to search: ");
+    scanf("%d", &val);
+    if(top==NULL)
+    {
+        printf("Stack is Empty\n");
+    }
+    else
+    {
+        while(ptr->info!=val && ptr!=NULL)
+        {
+            count++;
+            ptr=ptr->link;
+        }
+        if(count==0)
+        {
+            printf("Element Not Found\n");
+        }
+        else
+        {
+            printf("Element %d found in %d position\n", val, count+1);
+        }
 
-}
-
-// position Search block
-int indexElementSearch()
-{
+    }
 
 }
 
 // peek block
 int peek()
 {
-
+    if(top==NULL)
+    {
+        printf("Stack is empty\n");
+    }
+    else
+    {
+        printf("%d is the peak element\n", top->info);
+    }
+    
 }
 
 // display block
 int display()
 {
-    struct node *ptr;ptr=first;
-    printf("\n*****Your STACK Linkedlist Print Here*****\n");
-    while(ptr!=NULL)
+    struct node *ptr;
+    ptr=top;
+    if(top==NULL)
     {
-        printf("ptr-> %d \n",ptr->info);
-        ptr=ptr->link;
+        printf("Stack is Empty.\n");
+    }
+    else
+    {
+        printf("\n*****Your STACK Linkedlist Print Here*****\n");
+        while(ptr!=NULL)
+        {
+            printf("ptr-> %d \n",ptr->info);
+            ptr=ptr->link;
+        }
     }
 }
 
 // main block
 int main(){
-    int choice=0;
+    int choice;
     printf("***Stack Application starts from here***\n");
-    while(choice<6)
+    do
     {
         printf("\nPress '1' for push\n");
         printf("Press '2' for pop\n");
@@ -93,21 +137,7 @@ int main(){
             break;
             
             case 3:
-            printf("\n Press '1' to search for an element\n");
-            printf("\nPress '2' to search an element with specific index\n");
-            printf("\n Enter your choice: ");
-            scanf("%d", &choice);
-            //nested switch
-            switch(choice)
-            {
-                case 1:
-                elementSearch();
-                break;
-
-                case 2:
-                indexElementSearch();
-                break;
-            }
+            elementSearch();
             break;
 
             case 4:
@@ -118,7 +148,7 @@ int main(){
             display();
             break;
         }
-    }
+    }while(choice<6);
 
     printf("\nExiting the program...\n");
     return 0;
